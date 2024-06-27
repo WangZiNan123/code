9d48d5a1df8c7f4b211893f36e1cd877351e464d
WangZiNan123 <143243310+WangZiNan123@users.noreply.github.com>
2024-06-27 23:18:54 +0800

    Update 数据库存储数据.py

--- 数据库存储数据.py
+++ 数据库存储数据.py
@@ -3,7 +3,6 @@ from mysql.connector import Error
 import pandas as pd
 import numpy as np
 
-
 """
 版本：2024_6_27            版本时间：2024.6.27   
 更新内容：抓取自定义数据，存储到服务器数据库里面.数据库表头格式如下：
@@ -15,7 +14,9 @@ import numpy as np
         A_电堆电流(A),A_电堆功率(W),A1_电堆堆心温度(℃),A2_电堆堆心温度(℃),A1_电堆顶部温度(℃),A2_电堆顶部温度(℃),
         B_电堆状态,B_电堆电压(V),B_电堆电流(A),B_电堆功率(W),B_电堆堆心温度(℃),B1_电堆顶部温度(℃),B2_电堆顶部温度(℃),备注
     】
-
+    
+    自动获取数据表“备注”里面的最后17行，17-34行数据，进行对比。
+    
 """
 # 配置数据库连接参数
 db_config = {
@@ -61,19 +62,19 @@ try:
                                   `外置燃料(L)` DOUBLE,
                                   `内置燃料(L)` DOUBLE,
                                   `内置燃料(mm)` DOUBLE,
-    
+
                                   `A_制氢机状态` VARCHAR(100),
                                   `A_氢气压力(Psi)` DOUBLE,
                                   `A_鼓风机温度(℃)` DOUBLE,
                                   `A_提纯器温度(℃)` DOUBLE,
                                   `A_重整室温度(℃)` DOUBLE,
-    
+
                                   `B_制氢机状态` VARCHAR(100),
                                   `B_氢气压力(Psi)` DOUBLE,
                                   `B_鼓风机温度(℃)` DOUBLE,
                                   `B_提纯器温度(℃)` DOUBLE,
                                   `B_重整室温度(℃)` DOUBLE,
-    
+
                                   `A_电堆状态` VARCHAR(100),
                                   `A_电堆电压(V)` DOUBLE,
                                   `A_电堆电流(A)` DOUBLE,
@@ -82,7 +83,7 @@ try:
                                   `A2_电堆堆心温度(℃)` DOUBLE,
                                   `A1_电堆顶部温度(℃)` DOUBLE,
                                   `A2_电堆顶部温度(℃)` DOUBLE,
-    
+
                                   `B_电堆状态` VARCHAR(100),
                                   `B_电堆电压(V)` DOUBLE,
                                   `B_电堆电流(A)` DOUBLE,
@@ -90,9 +91,9 @@ try:
                                   `B_电堆堆心温度(℃)` DOUBLE,
                                   `B1_电堆顶部温度(℃)` DOUBLE,
                                   `B2_电堆顶部温度(℃)` DOUBLE,
-    
+
                                   `备注` TEXT,
-    
+
                                   PRIMARY KEY (`id`)
                                 )   """)
         sql = ("INSERT INTO COWIN_爬虫数据库 ("
@@ -170,8 +171,59 @@ try:
 
         # 提交事务
         conn.commit()
+
         tuples_list.clear()
+
         print("数据插入成功。")
+
+        table_name = 'COWIN_爬虫数据库'
+        column_name = '备注'
+        primary_key = 'id'
+        value17_34 = 'id'
+        # 编写SQL查询语句
+        # query = f"SELECT {column_name} FROM {table_name};"
+        # 编写SQL查询语句
+        # 编写SQL查询语句
+        query17_34 = f"SELECT {column_name} FROM {table_name} ORDER BY {value17_34} DESC LIMIT 17 OFFSET 17;"
+        query = f"SELECT {column_name} FROM {table_name} ORDER BY {primary_key} DESC LIMIT 17;"
+        # 执行查询
+        cursor.execute(query)
+        # 获取查询结果
+        results = cursor.fetchall()
+        # 执行查询
+        cursor.execute(query17_34)
+        # 获取查询结果
+        results17_34 = cursor.fetchall()
+
+        results_list17_34 = []
+        results_list = []
+        # 打印结果
+        for result in results:
+            if result[0] != '0':
+                results_list.append(result[0])
+        # print(f'最后插入的17个数故障表：{results_list}')
+
+        # 打印结果
+        for result in results17_34:
+            if result[0] != '0':
+                results_list17_34.append(result[0])
+
+        # 初始化一个标志变量，用于跟踪是否所有元素都相等
+        all_equal = True  # 假设所有元素都是相等的
+
+        if len(results_list) == len(results_list17_34):
+            for i in range(len(results_list)):
+                if results_list[i] != results_list17_34[i]:
+                    all_equal = False
+            if all_equal:
+                print(f'最后17个值 和 17-34的值是相等的：{results_list}')
+            else:
+                print(f'最后17个值 和 17-34的值是不相等的：{results_list}')
+        else:
+            print(f'最后17个值 和 17-34的值是不相等的：{results_list}')
+
+        results_list17_34.clear()
+        results_list.clear()
 
 except Error as e:
     print("数据库操作出错：", e)
