dc2c4e74035cdc2268cdf5f72c70b970b3aa6d2d
WangZiNan123 <143243310+WangZiNan123@users.noreply.github.com>
2024-06-28 09:56:28 +0800

    Update 数据库存储数据.py

--- 数据库存储数据.py
+++ 数据库存储数据.py
@@ -14,10 +14,19 @@ import numpy as np
         A_电堆电流(A),A_电堆功率(W),A1_电堆堆心温度(℃),A2_电堆堆心温度(℃),A1_电堆顶部温度(℃),A2_电堆顶部温度(℃),
         B_电堆状态,B_电堆电压(V),B_电堆电流(A),B_电堆功率(W),B_电堆堆心温度(℃),B1_电堆顶部温度(℃),B2_电堆顶部温度(℃),备注
     】
-    
+
     自动获取数据表“备注”里面的最后17行，17-34行数据，进行对比。
     
+版本：2024_6_28            版本时间：2024.6.28   
+更新内容：优化最后输出的逻辑 ，分新旧故障输出，对比最后17-34个的值与最后17个的值，如果相同，则输出旧故障，不同输出新故障
+
+
 """
+
+new_alarm = []  # 新的故障告警
+old_alarm = []  # 旧的故障告警
+same_alarm = []  # 相同的故障
+
 # 配置数据库连接参数
 db_config = {
     'host': '8.138.136.163',
@@ -146,7 +155,7 @@ try:
                "%s, %s, %s, %s, %s,%s, %s, %s,"
                "%s, %s, %s, %s, %s,%s, %s, %s, %s, %s)")
 
-        temp = pd.read_excel('E:/网页爬虫数据/网页采集数据_7.xlsx')
+        temp = pd.read_excel('D:/爬虫数据/网页采集数据_7.xlsx')
 
         # for i in range(len(temp.columns)):
         #     print(temp.columns[i],end=',')
@@ -204,23 +213,67 @@ try:
         # print(f'最后插入的17个数故障表：{results_list}')
 
         # 打印结果
-        for result in results17_34:
-            if result[0] != '0':
-                results_list17_34.append(result[0])
+        for result1 in results17_34:
+            if result1[0] != '0':
+                results_list17_34.append(result1[0])
 
-        # 初始化一个标志变量，用于跟踪是否所有元素都相等
-        all_equal = True  # 假设所有元素都是相等的
+        new_alarm.clear()
+        old_alarm.clear()
 
-        if len(results_list) == len(results_list17_34):
-            for i in range(len(results_list)):
-                if results_list[i] != results_list17_34[i]:
-                    all_equal = False
-            if all_equal:
-                print(f'最后17个值 和 17-34的值是相等的：{results_list}')
+        print(f'results_list元素：:{results_list}')
+        print(f'results_list17_34元素：{results_list17_34}')
+
+        # # 检查 '备注' 里面是否有故障
+        # if len(results_list) > 0:
+        #     # 将 results_list17_34 中的元素添加到一个临时列表中，用于后续比较
+        #     temp_list = results_list17_34.copy()
+        #
+        #     # 遍历 results_list 中的每个元素
+        #     for fault in results_list:
+        #         # 检查故障是否在 results_list17_34 中
+        #         if fault in temp_list:
+        #             # 如果是旧故障，添加到 old_alarm 列表
+        #             old_alarm.append(fault)
+        #             # 从 temp_list 中移除该故障，以避免重复判断
+        #             temp_list.remove(fault)
+        #         else:
+        #             # 如果是新故障，添加到 new_alarm 列表
+        #             new_alarm.append(fault)
+        #
+        # # 使用列表推导式去除 old_alarm 中的重复项
+        # old_alarm = [old_alarm[i] for n, i in enumerate(old_alarm) if old_alarm.index(old_alarm[i]) == n]
+        #
+        #     print(f'旧故障：{old_alarm}')
+        #     print(f'新故障：{new_alarm}')
+
+        # 检查 '备注' 里面是否有故障
+        if len(results_list) > 0:
+            # 检查是否有非 '0.0' 的元素
+            if any(element != '0.0' for element in results_list):
+                # 将 results_list17_34 中的所有元素添加到一个集合中，以提高查找效率
+                set_results_list17_34 = set(results_list17_34)
+
+                # 遍历 results_list 中的每个元素
+                for fault in results_list:
+                    # 如果该元素不在 results_list17_34 中，说明是新故障
+                    if fault not in set_results_list17_34:
+                        new_alarm.append(fault)
+                    else:
+                        # 如果该元素在 results_list17_34 中，说明是旧故障
+                        old_alarm.append(fault)
+
+                # 去除 old_alarm 中的重复项 。将列表转换成集合（set），自动去除重复元素，然后再将结果转换回列表。
+                # 集合是一个无序的数据结构，它不允许有重复的元素。
+                # old_alarm = list(set(old_alarm))
+                # 打印结果
+                print(f'旧故障：{old_alarm}')
+                print(f'新故障：{new_alarm}')
             else:
-                print(f'最后17个值 和 17-34的值是不相等的：{results_list}')
+                # 所有元素都是 '0.0'，没有故障，所以不执行任何操作
+                print("没有故障信息，无需计算。")
         else:
-            print(f'最后17个值 和 17-34的值是不相等的：{results_list}')
+
+            print(f'没有故障发生 ......')
 
         results_list17_34.clear()
         results_list.clear()
